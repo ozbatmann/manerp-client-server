f2e014651ffff00126cb985145df59b3c5fce0a8
Tunahan Bayindir <tunahan@endeksa.com>
2019-03-22 15:35:36 +0300

    Driver routes are configured

---
+++ client/src/modules/main/driver/route/index.js
@@ -0,0 +1,60 @@
+export const routes = {
+    information: 'driver.information',
+    orders: 'driver.routes',
+    documents: 'driver.documents'
+};
+
+const meta = (title) => {
+    return {
+        title: title,
+        tabbed: true,
+        tabs: [
+            {
+                text: 'genel',
+                to: { name: routes.information }
+            },
+            {
+                text: 'geçmiş rotalar',
+                to: { name: routes.orders }
+            },
+            {
+                text: 'dokümanlar',
+                to: { name: routes.documents }
+            }
+        ]
+    }
+};
+
+export default [
+    {
+        path: 'drivers',
+        name: 'drivers',
+        component: () => import('@/modules/main/driver/pages/MDriverManagement'),
+        meta: { title: 'Şoför Yönetimi' }
+    },
+    {
+        path: 'drivers/details/:id',
+        component: () => import('@/modules/main/shared/components/core/MCoreDetails'),
+        children: [
+            {
+                path: '',
+                name: routes.information,
+                component: () => import('@/modules/main/customer/pages/MCustomerDetailsInformation'),
+                meta: meta('Ahmet Aliakça')
+            },
+            {
+                path: 'routes',
+                name: routes.orders,
+                component: () => import('@/modules/main/customer/pages/MCustomerDetailsOrders'),
+                meta: meta('Ahmet Aliakça')
+            },
+            {
+                path: 'documents',
+                name: routes.documents,
+                component: () => import('@/modules/main/customer/pages/MCustomerDetailsDocuments'),
+                meta: meta('Ahmet Aliakça')
+            }
+        ]
+    }
+]
+
