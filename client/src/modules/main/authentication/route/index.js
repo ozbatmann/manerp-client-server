f38eba29f5eff73db1ce56cdee35d1479e4e2b67
Tunahan Bayindir <tunahan@endeksa.com>
2019-03-27 13:14:34 +0300

    Login authenticated check.

--- client/src/modules/main/authentication/route/index.js
+++ client/src/modules/main/authentication/route/index.js
@@ -4,10 +4,17 @@ export const routes = {
     login: 'login',
 };
 
+function authenticated () {
+    if (this.$store.shared.loginStatus) {
+        this.$router.push({ name: 'overview' })
+    }
+}
+
 export default [
     {
         path: 'login',
         name: routes.login,
+        beforeEnter: authenticated(),
         component: () => import('@/modules/main/authentication/pages/MLogin')
     },
 ]
