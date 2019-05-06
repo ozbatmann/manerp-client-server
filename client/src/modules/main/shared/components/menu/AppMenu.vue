40ead1c0948d81e69c9e5a830faa29e73da1add4
Tunahan Bayindir <tunahan.bayindir@ug.bilkent.edu.tr>
2019-05-06 05:09:28 +0300

    App Menu organisation name is dynamic now.

--- client/src/modules/main/shared/components/menu/AppMenu.vue
+++ client/src/modules/main/shared/components/menu/AppMenu.vue
@@ -23,9 +23,12 @@
                             <v-list-tile-content>
 
                                 <v-slide-x-transition>
-                                    <v-list-tile-title class="font-weight-light black--text text--accent-5">
+                                    <v-list-tile-title
+                                            class="font-weight-light black--text text--accent-5"
+                                            style="white-space: nowrap; text-overflow: ellipsis;"
+                                    >
 
-                                        Bumerang Lojistik
+                                        {{ organizationTitle }}
                                     </v-list-tile-title>
                                 </v-slide-x-transition>
                             </v-list-tile-content>
@@ -172,6 +175,14 @@
                 mini: false
             }
         },
+
+        computed: {
+            // TODO ellipsis...
+            organizationTitle () {
+                return this.$store.state.shared.organization.name;
+            }
+        },
+
         methods: {
         },
         watch: {
