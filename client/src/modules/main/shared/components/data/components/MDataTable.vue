97a120f23df766a5dd0dc4ec18086ad824f3ae02
Tunahan Bayindir <tunahan.bayindir@ug.bilkent.edu.tr>
2019-05-08 07:37:13 +0300

    fix

--- client/src/modules/main/shared/components/data/components/MDataTable.vue
+++ client/src/modules/main/shared/components/data/components/MDataTable.vue
@@ -168,7 +168,7 @@
                         :pagination.sync="pagination"
                         :loading="localLoading"
                         rows-per-page-text="Sayfa başına"
-                        :rows-per-page-items="50"
+                        :rows-per-page-items="[50]"
                         select-all
                         item-key="id"
                         class="m-data-table"
